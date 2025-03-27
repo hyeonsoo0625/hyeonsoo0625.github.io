66bd45918f74869e308e3802a4079a6e008e4dc7
hyeonsoo0625 <hyeonsoo02@gmail.com>
2025-03-28 01:59:34 +0900

    add build

--- src/pages/nlp/translate/TranslatePage.tsx
+++ src/pages/nlp/translate/TranslatePage.tsx
@@ -2,7 +2,6 @@ import { Background } from "@/entities";
 import { MainContainer } from "@/entities/screen/Container";
 import { TranslateService } from "@/shared";
 import { Header } from "@/widgets";
-import styled from "@emotion/styled";
 import { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 
@@ -43,25 +42,3 @@ const TranslatePage = () => {
 };
 
 export default TranslatePage;
-
-const FileContainer = styled.div`
-    width: 60vw;
-    height: 15vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-    background-color: #f0f0f0;
-    border-radius: 20px;
-    margin: 10px;
-    font-size: 1.5rem;
-    font-weight: 700;
-`;
-
-const Container = styled.div`
-    width: 100vw;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-`;
